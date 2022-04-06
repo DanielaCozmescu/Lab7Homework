a606a008a7a8d8320bf86e5f7a3d47fe5855f227
DanielaCozmescu <dani_21bcn@hotmail.com>
2022-04-06 17:26:35 +0300

    Homework2 updated

--- Homework2/src/ro/fasttrackit/lab7/Homework2.java
+++ Homework2/src/ro/fasttrackit/lab7/Homework2.java
@@ -2,6 +2,39 @@ package ro.fasttrackit.lab7;
 
 public class Homework2 {
     public static void main(String[] args) {
+        Product product1 = new Product("Sapun Lichid ", 10, 5, "Sanitare");
+        Product product2 = new Product("Balsam ", 18,20,  "Sanitare");
+        Product product3 = new Product("Piept pui ", 25, 0, "Carne");
+        Product product4 = new Product("Salam ", 86, 3, "Carne");
+        Product product5 = new Product("Sampon ", 16, 0, "Sanitare");
+        Product product6 = new Product("Perie ", 66, 0, " ");
+
+
+        printProduct(product1);
+        printProduct(product2);
+        printProduct(product3);
+        printProduct(product4);
+        printProduct(product5);
+        printProduct(product6);
+
+
+    }
+    public static void printProduct(Product product) {
+
+        if (product.isCategory("Carne")) {
+            System.out.println("Preparate din Carne");
+        } else if (product.isCategory("Sanitare")) {
+            System.out.println("Sanitare");
+        } else {
+            System.out.println("Produs neintrgistrat");
+        }
+
+        if (product.hasStock()) {
+            System.out.println(product.getName() + product.getPrice() + " Lei " + product.getQuantity() + " bucati " + "este in stoc.");
+        } else {
+            System.out.println(product.getName() + product.getPrice() + " Lei " + product.getQuantity() + " bucati " + "nu este in stoc.");
+        }
+
 
     }
 }
---
+++ Homework2/src/ro/fasttrackit/lab7/Product.java
@@ -0,0 +1,56 @@
+package ro.fasttrackit.lab7;
+
+public class Product {
+    private String name;
+    private int price;
+    private int quantity;
+    private String category;
+
+    public Product(String name, int price, int quantity, String category) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    boolean hasStock(){
+        return quantity != 0;
+    }
+
+    boolean isCategory(String category){
+        return this.category.equals(category);
+    }
+
+}
